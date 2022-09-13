32ee467a13e267e023522492eff18a6721c6cce8
prastya28 <prastya28@outlook.com>
2022-09-14 04:35:47 +0700

    hapus backup file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./gudang/**/*.{php,html,js}",
+    "./kasir/**/*.{php,html,js}",
+    "./index.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
