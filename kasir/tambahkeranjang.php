37176e7fdbb8ab7d3455dbbfb7302bb44b4beada
prastya28 <prastya28@outlook.com>
2023-06-07 00:14:58 +0700

    + validasi stok

--- kasir/tambahkeranjang.php
+++ kasir/tambahkeranjang.php
@@ -1,10 +1,20 @@
 <?php
 include '../koneksi.php';
 $id_produk = $_POST['id_produk'];
+$id_toko = $_SESSION['user']['id_toko'];
 
-// jika dikeranjang belum ada id produk itu, maka jumlahnya default 1
-if (!isset($_SESSION['keranjang'][$id_produk])) {
-    $_SESSION['keranjang'][$id_produk] =  1;
-} else {
-    $_SESSION['keranjang'][$id_produk] +=  1;
+$ambil = $koneksi->query("SELECT * FROM produk WHERE id_produk='$id_produk' AND id_toko='$id_toko' ");
+$produk = $ambil->fetch_assoc();
+
+if ($produk['stok_produk'] > 0) {
+    // jika dikeranjang belum ada id produk itu, maka jumlahnya default 1
+    if (!isset($_SESSION['keranjang'][$id_produk])) {
+        $_SESSION['keranjang'][$id_produk] =  1;
+    } else {
+        $jumlahdikeranjang = $_SESSION['keranjang'][$id_produk];
+
+        if ($produk['stok_produk'] > $jumlahdikeranjang) {
+            $_SESSION['keranjang'][$id_produk] +=  1;
+        }
+    }
 }
